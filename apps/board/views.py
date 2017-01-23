e85b261ce5b9d1e5bd1e22a6c70e1a70828a6023
Ronald Theodoro <ronald.silva4@fatec.sp.gov.br>
2017-01-22 23:22:41 -0200

    create Sprint, Task, User views set

--- apps/board/views.py
+++ apps/board/views.py
@@ -1,5 +1,28 @@
-from django.shortcuts import render
+from django.contrib.auth import get_user_model
+from rest_framework import viewsets
+from .models import Sprint
+from .models import Task
+from .serializers import SprintSerializer
+from .serializers import TaskSerializer
+from .serializers import UserSerializer
+from .mixin import DefaultsMixin
 
 
-def index(request):
-    return render(request, 'board/index.html')
+User = get_user_model()
+
+
+class SprintViewSet(DefaultsMixin, viewsets.ModelViewSet):
+    queryset = Sprint.objects.order_by('end')
+    serializer_class = SprintSerializer
+
+
+class TaskViewSet(DefaultsMixin, viewsets.ModelViewSet):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+
+
+class UserViewSet(DefaultsMixin, viewsets.ReadOnlyModelViewSet):
+    lookup_field = User.USERNAME_FIELD
+    lookup_url_kwarg = User.USERNAME_FIELD
+    queryset = User.object.order_by(User.USERNAME_FIELD)
+    serializer_class = UserSerializer
