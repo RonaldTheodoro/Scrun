efade5021a44d66704341deccd4a2846fcb150e1
Ronald Theodoro <ronald.silva4@fatec.sp.gov.br>
2017-01-25 16:59:25 -0200

    update tests

--- apps/board/tests/tests.py
+++ apps/board/tests/tests.py
@@ -4,13 +4,15 @@ from django.test import TestCase
 class ApiTest(TestCase):
 
     def test_get_root(self):
-        response = self.client.get('http://localhost:8000/api/')
+        response = self.client.get('http://localhost:8000/api/?format=json')
         self.assertEqual(200, response.status_code)
 
     def test_get_sprint(self):
-        response = self.client.get('http://localhost:8000/api/sprint/')
+        response = self.client.get(
+            'http://localhost:8000/api/sprints/?format=json')
         self.assertEqual(403, response.status_code)
 
     def test_get_task(self):
-        response = self.client.get('http://localhost:8000/api/task/')
+        response = self.client.get(
+            'http://localhost:8000/api/tasks/?format=json')
         self.assertEqual(403, response.status_code)
