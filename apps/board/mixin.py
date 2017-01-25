fc6a24093ab875b1e2f31bffacaed425eee73aef
Ronald Theodoro <ronald.silva4@fatec.sp.gov.br>
2017-01-25 02:17:37 -0200

    remove unused impor

--- apps/board/mixin.py
+++ apps/board/mixin.py
@@ -4,10 +4,9 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.filters import DjangoFilterBackend
 from rest_framework.filters import SearchFilter
 from rest_framework.filters import OrderingFilter
-from rest_framework.views import APIView
 
 
-class DefaultsMixin(APIView):
+class DefaultsMixin:
     authentication_classes = (BaseAuthentication, TokenAuthentication, )
     permission_classes = (IsAuthenticated, )
     paginate_by = 25
