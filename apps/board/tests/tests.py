c32395c956a1aaa33aa43adee78636d3adee1188
Ronald Theodoro <ronald.silva4@fatec.sp.gov.br>
2017-01-25 17:18:32 -0200

    rename tests

--- apps/board/tests/tests.py
+++ apps/board/tests/tests.py
@@ -2,17 +2,15 @@ from django.test import TestCase
 
 
 class ApiTest(TestCase):
-
+    URL = 'http://localhost:8000/api/{}'
     def test_get_root(self):
-        response = self.client.get('http://localhost:8000/api/?format=json')
+        response = self.client.get(self.URL.format('?format=json'))
         self.assertEqual(200, response.status_code)
 
-    def test_get_sprint(self):
-        response = self.client.get(
-            'http://localhost:8000/api/sprints/?format=json')
+    def test_get_sprint_fail(self):
+        response = self.client.get(self.URL.format('sprints/?format=json'))
         self.assertEqual(403, response.status_code)
 
-    def test_get_task(self):
-        response = self.client.get(
-            'http://localhost:8000/api/tasks/?format=json')
+    def test_get_task_fail(self):
+        response = self.client.get(self.URL.format('tasks/?format=json'))
         self.assertEqual(403, response.status_code)
