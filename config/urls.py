bebecc1b46ee51b419ea6012cc1b31bb4d0f078e
Ronald Theodoro <ronald.silva4@fatec.sp.gov.br>
2017-01-26 20:40:29 -0200

    finished chapter 04

--- config/urls.py
+++ config/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import url
 from django.conf.urls import include
+from django.views.generic import TemplateView
 from django.contrib import admin
 from rest_framework.authtoken.views import obtain_auth_token
 from apps.board.urls import router
@@ -8,6 +9,8 @@ from apps.board.urls import router
 urlpatterns = [
     # Admin
     url(r'^admin/', admin.site.urls),
+    # Index
+    url(r'^$', TemplateView.as_view(template_name='board/index.html')),
     # API
     url(r'^api/token/', obtain_auth_token, name='api-token'),
     url(r'^api/', include(router.urls)),
